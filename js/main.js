7404620a4b7569b4772c3f1b579e1f613693a2c0
Joan_E <joanevans18@gmail.com>
2020-09-11 14:37:15 +0300

    contacts update

--- js/main.js
+++ js/main.js
@@ -46,3 +46,30 @@ let saveMessage = (name, email, message) => {
         message:message
     })
 }
+let copyemail = () => {
+    let email = document.getElementById('myEmail')
+    email.select()
+    email.setSelectionRange(0, 99999)
+    document.execCommand('copy')
+    let tooltip = document.getElementById('tooltip')
+    let title = tooltip.getAttribute('title')
+    tooltip.setAttribute('title', `Copied: ${email.value}`)
+}
+
+$(document).ready(function () {
+    $('nav a[href*="#"]').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
+    $('#up').click(function() {      
+        $('body,html').animate({
+            scrollTop : 0                       
+        }, 500);
+    });
+    AOS.init({
+        easing: 'ease',
+        duration: 1000
+    });
+
+});
